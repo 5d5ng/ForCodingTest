9d3c6fc2d3a2c04c27c6caf5d85574b2fb4b8058
Dong-Eui Oh <deo1915@gmail.com>
2020-01-10 16:13:41 +0900

    commit

---
+++ 백준/Baekjoon/src/수학/ACM호텔.java
@@ -0,0 +1,29 @@
+package 수학;
+
+import java.util.Scanner;
+
+public class ACM호텔 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int  T = sc.nextInt();
+		int res = 0;
+		for(int i=0;i<T;i++) {
+			int h = sc.nextInt();
+			int w = sc.nextInt();
+			int n = sc.nextInt();
+
+			int rNum = n/h; //방번호 끝자리 rNum
+			if(n%h!=0) rNum++;
+			
+			int fNum = n%h; //층번호
+			if (fNum == 0)fNum = h;
+
+			res = 100*fNum+rNum;
+			System.out.println(res);
+
+		}
+
+	}
+
+}
