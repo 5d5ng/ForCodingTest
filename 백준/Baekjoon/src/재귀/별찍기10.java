3a8a38d4d86b8596da6cfd6d369568bc35f763be
Dong-eui Oh <deo1915@gmail.com>
2020-01-29 17:29:04 +0900

    fibo

---
+++ 백준/Baekjoon/src/재귀/별찍기10.java
@@ -0,0 +1,10 @@
+package Àç±Í;
+
+public class º°Âï±â10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- 백준/Baekjoon/src/재귀/피보나치수5.java
+++ 백준/Baekjoon/src/재귀/피보나치수5.java
@@ -4,40 +4,19 @@ import java.util.Scanner;
 
 public class 피보나치수5 {
 
-	
-	public static void main(String[] args) {
-		피보나치수5 app = new 피보나치수5();
-		app.process_command();
-	}
+	public static void main (String[] args) {
 
-	private  void process_command() {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-//		int ans = fibo(n,0);
-//		System.out.println(ans);
-		
-		int num1 = 0,num2 = 1,num3 = 0;
-		for(int i=0;i<n-1;i++) {
-			num3 = num1+num2;
-			num1 = num2;
-			num2 = num3;
-		}
-		
-		System.out.println(num3);
-		System.out.println(fibo(n,0,1,1));
-		
+		System.out.println(f(n));
+		sc.close();
 	}
-	int fibo(int limit,int n1,int n2,int n3) {
+	public static int f(int n) {
+		if(n==0) return 0;
+		else if(n==1) return 1;
 		
-		if(limit == 0) {
-			return n3;
+		else {
+			return f(n-1) + f(n-2);
 		}
-		else 
-			return fibo(limit-1,n2,n3,n1+n2)+fibo(limit-2,n2,n3,n1+n2);
-		
-		
-		
-		
 	}
-
 }
