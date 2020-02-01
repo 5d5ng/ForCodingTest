64102e93beb56fa77554a9f6dc85b5505de265c5
Dong-Eui Oh <deo1915@gmail.com>
2020-02-01 21:27:19 +0900

    The number of word

--- AlgorithmTest/BOJ/src/문자열/단어의개수.java
+++ AlgorithmTest/BOJ/src/문자열/단어의개수.java
@@ -4,16 +4,36 @@ import java.util.Scanner;
 
 public class 단어의개수 {
 
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int cnt = 0;
+//        String s = sc.nextLine();
+//        if (s.charAt(0) != ' ') cnt++;
+//        for (int i = 0; i < s.length() - 1; i++) {
+//            if (s.charAt(i) == ' ' && s.charAt(i + 1) != ' ') {
+//                cnt++;
+//            }
+//        }
+//        System.out.println(cnt);
+//    }
+
+    //다른 풀이
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int cnt = 0;
-        String s = sc.nextLine();
-        if (s.charAt(0) != ' ') cnt++;
-        for (int i = 0; i < s.length() - 1; i++) {
-            if (s.charAt(i) == ' ' && s.charAt(i + 1) != ' ') {
-                cnt++;
-            }
+        String input = sc.nextLine().trim(); // 입력받은 문자열의 앞과 뒤의 공백을 제거해주는 기능 : trim()
+        sc.close();
+
+
+        System.out.println(input);
+        if(input.isEmpty())
+        {
+            System.out.println(0);
+
         }
-        System.out.println(cnt);
+        else{
+            System.out.println(input.split(" ").length);
+        }
+
     }
+
 }
