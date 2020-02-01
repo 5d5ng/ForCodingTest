c65cffaa680cf8082e44305efdb15a000a3bfd0b
Dong-Eui Oh <deo1915@gmail.com>
2020-02-01 21:18:14 +0900

    The number of word

---
+++ AlgorithmTest/BOJ/src/문자열/단어의개수.java
@@ -0,0 +1,19 @@
+package 문자열;
+
+import java.util.Scanner;
+
+public class 단어의개수 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = 0;
+        String s = sc.nextLine();
+        if (s.charAt(0) != ' ') cnt++;
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == ' ' && s.charAt(i + 1) != ' ') {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
