c05265063280c332c46cb66a6eae9be57932f99e
Dong-eui Oh <deo1915@gmail.com>
2020-02-02 23:58:50 +0900

    Feb_2nd

--- 백준/Baekjoon/src/재귀/별찍기10.java
+++ 백준/Baekjoon/src/재귀/별찍기10.java
@@ -1,10 +1,19 @@
 package Àç±Í;
 
+import java.util.Scanner;
+
 public class º°Âï±â10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		sc.close();
+	}
+	public static void f(int n) {
+		
+		System.out.print("*");
 	}
 
 }
