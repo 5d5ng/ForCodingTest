bf687074cb682fb1f997450d62bdeb141ee22344
dongeui <deo1915@gmail.com>
2020-02-18 15:49:33 +0900

    Feb18

--- AlgorithmTest/BOJ/src/정렬/나이순_정렬.java
+++ AlgorithmTest/BOJ/src/정렬/나이순_정렬.java
@@ -1,4 +1,48 @@
 package 정렬;
+import java.io.*;
+import java.util.*;
+class Client{
+    int index ;
+    int age;
+    String name;
 
+    public Client(int age, String name,int index) {
+
+        this.age = age;
+        this.name = name;
+        this.index = index;
+    }
+}
 public class 나이순_정렬 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int index = 0;
+        ArrayList<Client> list = new ArrayList<Client>();
+        for (int i = 0; i < n; i++) {
+            String [] input = br.readLine().split(" ");
+            int inputAge = Integer.parseInt(input[0]);
+            String inputName = input[1];
+            list.add(new Client(inputAge,inputName,index++));
+        }
+        Comparator<Client> c = new Comparator<Client>(){
+            @Override
+            public int compare(Client c1,Client c2){
+                if(c1.age > c2.age) return 1;
+                else if(c1.age == c2.age){
+                    if(c1.index>c2.index){
+                        return 1;
+                    }
+                    else return 0;
+                }
+                return -1;
+            }
+        };
+        list.sort(c);
+        for(Client client:list){
+            System.out.println(client.age+" "+client.name);
+        }
+
+    }
 }
--- AlgorithmTest/BOJ/src/정렬/좌표정렬하기2.java
+++ AlgorithmTest/BOJ/src/정렬/좌표정렬하기2.java
@@ -1,33 +1,35 @@
 package 정렬;
 import java.io.*;
 import java.util.*;
-class Point{
+
+class Point2{
     int x,y;
 
-    public Point(int x, int y)   {
+    public Point2(int x, int y)   {
         this.x = x;
         this.y = y;
     }
 }
-public class 좌표정렬하기 {
+
+public class 좌표정렬하기2 {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Comparator<Point> com = null;
-        ArrayList<Point> list = new ArrayList<Point>();
-        com = new Comparator<Point>() {
+        Comparator<Point2> com = null;
+        ArrayList<Point2> list = new ArrayList<Point2>();
+        com = new Comparator<Point2>() {
             @Override
-            public int compare(Point o1, Point o2) {
-                if (o1.x > o2.x) return 1;
-                else if (o1.x == o2.x) {
-                    if (o1.y > o2.y) return 1;
+            public int compare(Point2 o1, Point2 o2) {
+                if (o1.y > o2.y) return 1;
+                else if (o1.y == o2.y) {
+                    if (o1.x > o2.x) return 1;
                     else return -1;
                 } else return -1;
             }
         };
         for (int i = 0; i < n; i++) {
             String[] in = br.readLine().split(" ");
-            list.add(new Point(Integer.parseInt(in[0]), Integer.parseInt(in[1])));
+            list.add(new Point2(Integer.parseInt(in[0]), Integer.parseInt(in[1])));
         }
         list.sort(com);
         for (int i = 0; i < list.size(); i++) {
