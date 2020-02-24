e1474dbaa7d504a532c87b91c970f915b23994bf
dongeui <deo1915@gmail.com>
2020-02-24 14:39:32 +0900

    Feb24

--- AlgorithmTest/BOJ/src/백트래킹/연산자_끼워넣기.java
+++ AlgorithmTest/BOJ/src/백트래킹/연산자_끼워넣기.java
@@ -1,22 +1,14 @@
 package 백트래킹;
-
-import jdk.nashorn.internal.runtime.FindProperty;
-
 import java.io.*;
-import java.util.*;
-import java.util.PriorityQueue;
 
 public class 연산자_끼워넣기 {
     static int n;
     static int[] op = new int[4];
     static int[] num;
-    static ArrayList<Integer> list = new ArrayList<>();
-    static int res = 0;
-    static ArrayList<Integer> OrderOp = new ArrayList<>();
+    static int  max = Integer.MIN_VALUE,min = Integer.MAX_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-
         num = new int[n];
         String[] in = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
@@ -26,38 +18,36 @@ public class 연산자_끼워넣기 {
         for (int i = 0; i < 4; i++) {
             op[i] = Integer.parseInt(in[i]);
         }
-        res = num[0];
-        fun(0);
-        Collections.sort(list);
-        System.out.println(list.get(list.size()-1));
-        System.out.println(list.get(0));
-
+        fun(1,num[0]);
+        System.out.println(max);
+        System.out.println(min);
+        br.close();
     }
-    public static void fun(int n1) {
-       if(n1>=n-1){
-           list.add(res);
-           res = num[0];
-           return;
-       }
-        for (int i = 0; i <4 ; i++) {
-          if(op[i]>0){
-              op[i]--;
-              res = Compute(res,num[n1+1],i);
-              fun(n1+1);
-              op[i]++;
-          }
+    public static void fun(int depth,int ans){
+        if(depth == n){
+            max = Math.max(ans,max);
+            min = Math.min(ans,min);
+            return;
         }
-    }
-    public static int Compute(int a,int b,int op){
-        if(op==0) return a+b;
-        else if(op==1) return a-b;
-        else if(op==2) return a*b;
-        else  return a/b;
-    }
-    public static void dfs(int depth,int sum){
         for (int i = 0; i < 4; i++) {
-            
-            
+            if(op[i]>0){
+                op[i]--;
+                if(i==0){
+                    fun(depth+1,ans+num[depth]);
+                }
+                else if(i==1){
+                    fun(depth+1,ans-num[depth]);
+                }
+                else if(i==2){
+                    fun(depth+1,ans*num[depth]);
+                }
+                else {
+                    fun(depth+1,ans/num[depth]);
+                }
+                op[i]++;
+            }
         }
     }
+
+
 }
