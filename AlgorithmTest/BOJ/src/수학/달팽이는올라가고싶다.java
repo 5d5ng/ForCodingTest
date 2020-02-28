712ca21aadc78f367847c7746d776942aeb02d0b
dongeui <deo1915@gmail.com>
2020-02-28 14:28:07 +0900

    Feb28

--- AlgorithmTest/BOJ/src/수학/달팽이는올라가고싶다.java
+++ AlgorithmTest/BOJ/src/수학/달팽이는올라가고싶다.java
@@ -1,18 +1,27 @@
 package 수학;
 
+import java.util.Random;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class 달팽이는올라가고싶다 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double a = sc.nextLong();
-        double b = sc.nextLong();
-        double v = sc.nextLong();
-        if(v==a) System.out.println(1);
-        else {
-            long day = (long) Math.ceil((v-a)/(a-b)) +1 ;
-            System.out.println(day);
-        }
+//        Scanner sc = new Scanner(System.in);
+//        double a = sc.nextLong();
+//        double b = sc.nextLong();
+//        double v = sc.nextLong();
+//        if(v==a) System.out.println(1);
+//        else {
+//            long day = (long) Math.ceil((v-a)/(a-b)) +1 ;
+//            System.out.println(day);
+//        }
+       String s ="qwe wqe   ww";
+       StringTokenizer st = new StringTokenizer(s);
+        System.out.println(st.nextToken());
+        System.out.println(st.nextToken());
+        System.out.println(st.nextToken());
+
+
 
     }
 
