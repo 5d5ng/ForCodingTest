7892943597e96bb4f2ec71168fe373063941e1c6
dongeui <deo1915@gmail.com>
2020-03-11 23:10:46 +0900

    Mar11

--- AlgorithmTest/BOJ/src/DFS_BFS/DFS와BFS_인접리스트.java
+++ AlgorithmTest/BOJ/src/DFS_BFS/DFS와BFS_인접리스트.java
@@ -6,15 +6,13 @@ class Node {
     LinkedList<Integer> list;
     int data;
     boolean visited;
-    int pi ;
     public Node(int data) {
         this.data = data;
         list = new LinkedList<>();
     }
 }
 
-public class DFS와BFS {
-
+public class DFS와BFS_인접리스트 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -29,10 +27,37 @@ public class DFS와BFS {
             st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            G[u].list.addFirst(v);
-            G[v].list.addFirst(u);
+            G[u].list.add(v);
+            G[v].list.add(u);
+        }
+        for (int i = 1; i <G.length ; i++) {
+            Collections.sort(G[i].list);
         }
         dfs(G, startD);
+        System.out.println();
+        for (int i = 1; i <G.length ; i++) {
+            G[i].visited = false;
+        }
+        bfs(G,startD);
+    }
+
+    private static void bfs(Node[] G, int startD) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(startD);
+        G[startD].visited = true;
+
+        while(!q.isEmpty()){
+            int v = q.poll();
+            System.out.print(v+" ");
+            for(int i:G[v].list){
+                if(!G[i].visited){
+                    q.add(i);
+                    G[i].visited = true;
+                }
+            }
+        }
+
+
     }
 
     private static void dfs(Node[] G, int start) {
