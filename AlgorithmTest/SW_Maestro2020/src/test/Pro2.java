80900d5aa4985b470b7e8528e63444887e2f6487
Dong-eui Oh <deo1915@gmail.com>
2020-03-16 16:00:58 +0900

    Mar16

--- AlgorithmTest/SW_Maestro2020/src/test/Pro2.java
+++ AlgorithmTest/SW_Maestro2020/src/test/Pro2.java
@@ -3,53 +3,51 @@ package test;
 import java.io.*;
 import java.sql.SQLOutput;
 import java.util.*;
-
 public class Pro2 {
     static int[][] tile;
     static int[][] goal;
     static int m = 0, n = 0;
-
+    static boolean flag = false;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-
         StringTokenizer st;
         for (int i = 0; i < t; i++) {
             st = new StringTokenizer(br.readLine());
             n = Integer.parseInt(st.nextToken());
             m = Integer.parseInt(st.nextToken());
-            tile = new int[n+1][m+1];
-            goal = new int[n+1][m+1];
+            tile = new int[n + 1][m + 1];
+            goal = new int[n + 1][m + 1];
             for (int j = 1; j <= n; j++) {
                 st = new StringTokenizer(br.readLine());
                 for (int k = 1; k <= m; k++) {
                     goal[j][k] = Integer.parseInt(st.nextToken());
                 }
             }
-            if (DFS(1, 1)) System.out.println("YES");
+            DFS(1,1);
+            if (flag) System.out.println("YES");
             else System.out.println("NO");
+            flag = false;
 
         }
 
     }
-
-    static void Paint(int i, int j) {
-        for (; i < n && i < i + 2; i++) {
-            for (; j < m && j < j + 2; j++) {
+    static void Paint(int x, int y) {
+        for (int i = x; i <= n && i < x + 2; i++) {
+            for (int j = y; j <= m && j < y + 2; j++) {
                 tile[i][j] = 1;
             }
         }
     }
-
-    static void DePaint(int i, int j) {
-        for (; i < n && i < i + 2; i++) {
-            for (; j < m && j < j + 2; j++) {
+    static void DePaint(int x, int y) {
+        for (int i = x; i <= n && i < x + 2; i++) {
+            for (int j = y; j <= m && j < y + 2; j++) {
                 tile[i][j] = 0;
+
             }
         }
     }
-
-    static Boolean chkSame() {
+    static boolean chkSame() {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 if (tile[i][j] != goal[i][j]) return false;
@@ -57,26 +55,24 @@ public class Pro2 {
         }
         return true;
     }
-
-    static boolean DFS(int x, int y) {
-        if(x==n||y==m){
-            return chkSame();
+    static void DFS(int x, int y) {
+        if (chkSame()) {
+           flag = true;
+           return;
         }
-
-        Paint(x,y);
-        DFS(x+1,y);
-        DePaint(x,y);
-
-        Paint(x,y);
-        DFS(x,y+1);
-        DePaint(x,y);
-
-        Paint(x,y);
-        DFS(x+1,y+1);
-        DePaint(x,y);
-
-        return false;
+        if (x == n && y == m) {
+            return ;
+        }
+        if (y == m) {
+            y = 1;
+            x++;
+        }
+        //색칠안하는부분
+        DFS(x, y + 1);
+        //색칠한다
+        Paint(x, y);
+        DFS(x, y + 1);
+        DePaint(x, y);
     }
-
 
 }
