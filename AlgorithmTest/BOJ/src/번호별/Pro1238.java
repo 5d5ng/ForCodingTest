fedfda337bfa6f3d5d41d64d61094084e5a9e007
Dong-eui Oh <deo1915@gmail.com>
2020-04-07 00:54:18 +0900

    Apr07

--- AlgorithmTest/BOJ/src/번호별/Pro1238.java
+++ AlgorithmTest/BOJ/src/번호별/Pro1238.java
@@ -1,46 +1,70 @@
 package 번호별;
+
 import java.io.*;
 import java.util.*;
-class City{
-    int num;
-    ArrayList<int[]> AdList = new ArrayList<>();
-    City(int num){
-        this.num = num;
-    }
-}
+
 public class Pro1238 {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n= Integer.parseInt(st.nextToken()),m= Integer.parseInt(st.nextToken()),x = Integer.parseInt(st.nextToken());
-        City[] list = new City[n+1];
-        for (int i = 1; i < n; i++) {
-            list[i] = new City(i);
-        }
-        for (int i = 0; i <m ; i++) {
+        int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken()), x = Integer.parseInt(st.nextToken());
+        int[][] Arr = new int[n + 1][n + 1];
+
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
             int dis = Integer.parseInt(st.nextToken());
-            int[] temp = {end,dis};
-            list[start].AdList.add(temp);
+            Arr[start][end] = dis;
+        }
+        int ans = -1;
+        for (int i = 1; i < n + 1; i++) {
+            boolean[] vis = new boolean[n + 1];
+            int[] dArr = new int[n + 1];
+             ans = Math.max(ans,fun(Arr, dArr, vis, i, x) + fun(Arr,dArr,vis,x,i));
+
         }
 
+        System.out.println(ans);
 
     }
-    static void fun(City[] list,int x){
-        for(int i=1;i<list.length;i++) {
-            PriorityQueue<int[]> q = new PriorityQueue<>(new Comparator<int[]>() {
-                @Override
-                public int compare(int[] o1, int[] o2) {
-                    return o1[1]-o1[2];
+
+    static int fun(int[][] Arr, int[] dArr, boolean[] vis, int start, int endIndex) {
+        for (int i = 1; i < dArr.length; i++) {
+            dArr[i] = Integer.MAX_VALUE;
+            vis[i] = false;
+        }
+        dArr[start] = 0;
+        vis[start] = true;
+        for (int i = 1; i < Arr.length; i++) {
+            if (!vis[i] && Arr[start][i] > 0) { //인접노드
+                dArr[i] = Arr[start][i];
+            }
+        }
+        for (int i = 1; i < Arr.length - 1; i++) { //모든 노드 돌아보기
+            int min = Integer.MAX_VALUE;
+            int index = -1;
+
+            for (int j = 1; j < Arr.length; j++) {
+                if (!vis[j] && dArr[j] != Integer.MAX_VALUE) { //한번도 방문한적없고 인접한경우
+                    if (dArr[j] < min) {
+                        min = dArr[j];
+                        index = j;
+                    }
                 }
-            });
-            for(int[] temp: list[i].AdList){
-                q.add(temp);
+            }
+            vis[index] = true;
+            for (int j = 1; j < Arr.length; j++) {
+                if (!vis[j] && Arr[index][j] > 0) {
+                    if (dArr[j] > dArr[index] + Arr[index][j]) {
+                        dArr[j] = dArr[index] + Arr[index][j];
+                    }
+                }
             }
 
-
         }
+        return dArr[endIndex];
     }
+
 }
