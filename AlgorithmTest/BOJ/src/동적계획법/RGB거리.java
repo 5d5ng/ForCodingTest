445fada11a2c155f923b78c9e66f63a2a4b8af8c
Dong-eui Oh <deo1915@gmail.com>
2020-04-28 21:36:23 +0900

    Apr28

--- AlgorithmTest/BOJ/src/동적계획법/RGB거리.java
+++ AlgorithmTest/BOJ/src/동적계획법/RGB거리.java
@@ -9,45 +9,26 @@ public class RGB거리 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int[] dpIndex = new int[N + 1];
-        int[] dp = new int[N+1];
-        int[][] rgb = new int[N + 1][3];
+
+        int[][] dp = new int[N + 1][3]; //같은 색을 연속할 수 없기 때문에 다른 두가지 색중 최소값만 더한다
+        int[][] rgb = new int[N + 1][3]; //비용 저장
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
                 rgb[i][j] = Integer.parseInt(st.nextToken());
             }
-
         }
-        int min = Integer.MAX_VALUE;
+
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                for (int k = 0; k < 3; k++) {
-                    if(i!=j && j!=k){
-                        int t = rgb[1][i]+rgb[2][j]+rgb[3][k];
-                        if(min > t ){
-                            min = t;
-                            dpIndex[1] = i;
-                            dpIndex[2] = j;
-                            dpIndex[3] = k;
-                            dp[1] = rgb[1][i];
-                            dp[2] = dp[1] +rgb[2][j];
-                            dp[3] = dp[2] +rgb[3][k];
-                        }
-                    }
-                }
-            }
+            dp[1][i] = rgb[1][i];
         }
-        for(int i=4;i<=N;i++){
-            dp[i] = Integer.MAX_VALUE;
-            for(int j=0;j<3;j++){
-                if(dpIndex[i-1]!=j && dp[i] > dp[i-1]+rgb[i][j]){
-                    dp[i] = dp[i-1] +rgb[i][j];
-                }
-            }
-
+        for(int i=2;i<=N;i++){
+            dp[i][0] = Math.min(dp[i-1][1],dp[i-1][2])+rgb[i][0];
+            dp[i][1] = Math.min(dp[i-1][0],dp[i-1][2])+rgb[i][1];
+            dp[i][2] = Math.min(dp[i-1][0],dp[i-1][1])+rgb[i][2];
         }
+        int res = Math.min(Math.min(dp[N][0],dp[N][1]),dp[N][2]);
+        System.out.println(res);
 
-        System.out.println(dp[N]);
     }
 }
