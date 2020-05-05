a13de75e2758aa22eefab3af96de8546735ee72b
Dong-eui Oh <deo1915@gmail.com>
2020-05-05 18:57:16 +0900

    May05

--- AlgorithmTest/BOJ/src/동적계획법/포도주시식.java
+++ AlgorithmTest/BOJ/src/동적계획법/포도주시식.java
@@ -6,27 +6,20 @@ public class 포도주시식 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] ar = new int[n + 1];
-        int[][] dp = new int[n+1][n+1];
+        int[] ar = new int[n +1];
+        int[] dp = new int[n+1];
         for (int i = 1; i <= n; i++) {
             ar[i] = Integer.parseInt(br.readLine());
         }
-        for(int i=1;i<=n;i++)
-        {
-            dp[i][1] = ar[i];
-            dp[i][2] = dp[i-1][1] + ar[i];
-        }
-        for (int i = 3; i <=n ; i++) {
-            for(int j=3; j<=n;j++){
-                dp[i][j] = dp[i-1][j-2] +dp[i][1];
-            }
-        }
-        int res = 0;
-        for (int i = 1; i <=n ; i++) {
-            res =Math.max(dp[n][i],res);
-        }
-        System.out.println(res);
 
-
+        dp[1] = ar[1];
+        dp[2] = ar[1]+ar[2];
+        dp[3] = Math.max(ar[2]+ar[3],dp[1]+ar[3]);
+        dp[3] = Math.max(dp[2],dp[3]);
+        for(int i = 4;i<=n;i++){
+            dp[i] = Math.max(dp[i-2]+ar[i],dp[i-3]+ar[i-1]+ar[i]); //점화식
+            dp[i] = Math.max(dp[i-1],dp[i]); //현재차례의 주스를 선택안하는 것이 더 손해인 경우생각
+        }
+        System.out.println(dp[n]);
     }
 }
