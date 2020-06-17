74a9f4c1fa603c366a297c9b86fdce863ec8159a
5d5ng <deo1915@gmail.com>
2020-06-17 13:06:11 +0900

    Jun17

--- Python/Programmers/Lv2/행렬의 곱셈.py
+++ Python/Programmers/Lv2/행렬의 곱셈.py
@@ -12,10 +12,25 @@ def solution(arr1, arr2):
 
 ar1 = [[1,4],[3,2],[4,1]]
 ar2 = [[3,3],[3,3]]
-a = solution(ar1,ar2)
-print(a)
+# a = solution(ar1,ar2)
+# print(a)
 
 
 
 def productMatrix(A, B):
     return [[sum(a*b for a, b in zip(A_row,B_col)) for B_col in zip(*B)] for A_row in A]
+
+def solution2(A,B):
+    ans =[]
+    for A_row in A:
+        lst = []
+        for B_row in zip(*B):
+            print(B_row)
+            
+            temp =  sum (a*b for a,b in zip(A_row,B_row) )
+            lst.append(temp)
+        ans.append(lst)
+    return ans
+
+an  = solution2(ar1,ar2)
+print(an)        
