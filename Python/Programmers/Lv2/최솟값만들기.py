1956bdc2c21669bc81e7ffdebf13c6bd0c9f55fd
5d5ng <deo1915@gmail.com>
2020-06-19 19:13:44 +0900

    Jun19

---
+++ Python/Programmers/Lv2/최솟값만들기.py
@@ -0,0 +1,20 @@
+
+def solution(A,B):
+    ans = 0
+    A.sort()
+    B.sort()
+    B.reverse()
+    for i,j in zip(A,B):
+        ans+=(i*j)
+    return ans
+
+
+arr = [1,2,3]
+arr2  = [2,3,4]
+arr2.sort()
+arr2.reverse()
+print(arr2)
+
+print(solution(arr,arr2))
+# 
+
