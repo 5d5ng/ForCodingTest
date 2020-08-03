9154b63edc595aa8f50bbf1cbc67eefeba8e3c16
5d5ng <deo1915@gmail.com>
2020-08-03 11:23:33 +0900

    다른 사람 풀이

--- Python/LeetCode/easy/617_Merge_Two_Binary_Trees.py
+++ Python/LeetCode/easy/617_Merge_Two_Binary_Trees.py
@@ -11,7 +11,7 @@ class Solution(object):
 # class Solution(object):
 #     def mergeTrees(self, t1, t2):
 #         if t1 and t2:
-#             t1.val +=t2.vak
+#             t1.val +=t2.val
 #             t1.left = self.mergeTrees(t1.left,t2.left)
 #             t1.right = self.mergeTrees(t1.right,t2.right)
 #         else:
