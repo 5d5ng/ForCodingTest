f1617b7e546474e509c25a18d2d2021c31126f86
5d5ng <deo1915@gmail.com>
2020-08-03 11:14:13 +0900

    Merge Two Binary Trees completed

--- AlgorithmTest/LeetCode/src/easy/Merge_two_BinaryTree617.java
+++ AlgorithmTest/LeetCode/src/easy/Merge_two_BinaryTree617.java
@@ -1,4 +1,33 @@
 package easy;
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {
+    }
+    TreeNode(int val) {
+        this.val = val;
+    }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class Merge_two_BinaryTree617 {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+
+        if (t1 == null) return t2;
+        else if (t2 == null) return t1;
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+
+        return t1;
+
+    }
+
+
 }
