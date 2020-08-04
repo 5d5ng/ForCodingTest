46fe5fc676c5e684ac74cd58362f84b7ee50674c
5d5ng <deo1915@gmail.com>
2020-08-04 13:28:10 +0900

    104 leetcode

--- AlgorithmTest/LeetCode/src/easy/Maximum_Depth_of_Binary_Tree.java
+++ AlgorithmTest/LeetCode/src/easy/Maximum_Depth_of_Binary_Tree.java
@@ -1,4 +1,26 @@
 package easy;
-
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(){}
+    TreeNode(int val){
+        this.val = val;
+    }
+    public TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 public class Maximum_Depth_of_Binary_Tree {
+
+    public static void main(String[] args) {
+
+    }
+    public int maxDepth(TreeNode root) {
+        if(root ==null) return 0;
+        return Math.max(maxDepth(root.left)+1,maxDepth(root.right)+1);
+    }
+
 }
