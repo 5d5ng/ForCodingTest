bd56af25a70c9ec3e19dd41490a5ae99fb7ddcaa
5d5ng <deo1915@gmail.com>
2020-08-07 00:06:25 +0900

    invert binary tree

---
+++ AlgorithmTest/LeetCode/src/easy/Invert_Binary_Tree226.java
@@ -0,0 +1,4 @@
+package easy;
+
+public class Invert_Binary_Tree226 {
+}
---
+++ AlgorithmTest/LeetCode/src/easy/Maximum_Depth_of_Binary_Tree104.java
@@ -0,0 +1,13 @@
+package easy;
+
+public class Maximum_Depth_of_Binary_Tree {
+
+    public static void main(String[] args) {
+
+    }
+    public int maxDepth(TreeNode root) {
+        if(root ==null) return 0;
+        return Math.max(maxDepth(root.left)+1,maxDepth(root.right)+1);
+    }
+
+}
