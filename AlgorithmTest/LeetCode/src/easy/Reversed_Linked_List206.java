10b060e71136a65778b4e4d3a65c818a4f8838fc
5d5ng <deo1915@gmail.com>
2020-08-09 14:29:04 +0900

    Reversed Linked List

--- AlgorithmTest/LeetCode/src/easy/Reversed_Linked_List206.java
+++ AlgorithmTest/LeetCode/src/easy/Reversed_Linked_List206.java
@@ -1,4 +1,23 @@
 package easy;
 
+import java.util.ArrayList;
+
 public class Reversed_Linked_List206 {
+
+    public ListNode reverseList(ListNode head){
+        ArrayList<Integer> list = new ArrayList<>();
+        ListNode ans = new ListNode();
+        while(head!=null){
+            list.add(head.val);
+            head = head.next;
+        }
+        ListNode temp = ans;
+        while (!list.isEmpty()){
+            temp.val = list.remove(list.size()-1);
+            temp.next = new ListNode();
+            temp = temp.next;
+        }
+        return ans;
+    }
 }
+
